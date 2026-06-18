e5e66c81c3544eb506600dce020c7ab618d25e3a
Tagadinamani <Prajwal.Tagadinamani@yokogawa.com>
2026-06-18 09:37:14 +0530

    Create render admin

--- manage.py
+++ manage.py
@@ -6,14 +6,37 @@ import sys
 
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
     try:
         from django.core.management import execute_from_command_line
+
+        # Create Render admin user automatically if it doesn't exist
+        try:
+            import django
+            django.setup()
+
+            from django.contrib.auth import get_user_model
+
+            User = get_user_model()
+
+            if not User.objects.filter(username="renderadmin").exists():
+                User.objects.create_superuser(
+                    username="renderadmin",
+                    email="admin@example.com",
+                    password="Render@123"
+                )
+                print("Render admin created")
+
+        except Exception as e:
+            print(f"Admin creation skipped: {e}")
+
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
